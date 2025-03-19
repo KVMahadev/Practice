6162b8b496ec65134af9bedb3b10a1724c02b08e
KVMahadev <someshwarankrishna@gmail.com>
2025-03-19 17:15:01 +0530

    second commit

---
+++ src/page/java/Homepage.java
@@ -0,0 +1,13 @@
+package page.java;
+
+public class Homepage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is tesitng");
+		}
+
+	}
+
+
---
+++ src/test/login.java
@@ -0,0 +1,13 @@
+package test;
+
+public class login {
+	
+	public static void check() {
+		System.out.println("from test package");
+	}
+
+	public static void main(String args[]) {
+		check();
+		
+	}
+}
