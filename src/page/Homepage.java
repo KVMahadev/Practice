cb727b469c694f74eab21946ed21996dea0ff4d8
KVMahadev <someshwarankrishna@gmail.com>
2025-03-19 23:25:49 +0530

    4th push

--- src/page/Homepage.java
+++ src/page/Homepage.java
@@ -1,9 +1,10 @@
-package page.java;
+package page;
 
 public class Homepage {
 	
 	public void check() {
 		System.out.println("Practice again");
+		System.out.println("GIT Work");
 	}
 
 	public static void main(String[] args) {
---
+++ src/page/Regpage.java
@@ -0,0 +1,12 @@
+package page;
+
+public class Regpage {
+
+	String name = "check";
+	int n = 2;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
