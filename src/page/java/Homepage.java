1bb9ae941853e73cd50a8b62da3641263f9af22d
KVMahadev <someshwarankrishna@gmail.com>
2025-03-19 17:26:08 +0530

    third commit

--- src/page/java/Homepage.java
+++ src/page/java/Homepage.java
@@ -1,9 +1,14 @@
 package page.java;
 
 public class Homepage {
+	
+	public void check() {
+		System.out.println("Practice again");
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 		
 		System.out.println("This is tesitng");
 		}
--- src/test/login.java
+++ src/test/login.java
@@ -4,6 +4,7 @@ public class login {
 	
 	public static void check() {
 		System.out.println("from test package");
+		String A = "test";
 	}
 
 	public static void main(String args[]) {
